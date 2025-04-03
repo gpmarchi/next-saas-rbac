def6a9b28bb10592e5a3a98671c0a569f2452ea7
gpmarchi <gustavomarchi@gmail.com>
2025-04-03 18:09:55 -0300

    feat: get signed in users' permissions to show or not project switcher

--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -1,9 +1,9 @@
 import { ChevronsUpDown, PlusCircle } from 'lucide-react'
-import { cookies } from 'next/headers'
 import Link from 'next/link'
 
 import { getOrganizations } from '@/http/get-organizations'
 
+import { getCurrentOrg } from '@/auth/auth'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import {
   DropdownMenu,
@@ -16,8 +16,7 @@ import {
 } from './ui/dropdown-menu'
 
 export async function OrganizationSwitcher() {
-  const cookieStore = await cookies()
-  const currentOrg = cookieStore.get('org')?.value
+  const currentOrg = await getCurrentOrg()
 
   const { organizations } = await getOrganizations()
 
--- packages/auth/src/models/user.ts
+++ packages/auth/src/models/user.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import { roleSchema } from '../roles'
 
 export const userSchema = z.object({
-  __typename: z.literal('User').default('User'),
+  __typename: z.literal('User').default('User').optional(),
   id: z.string(),
   role: roleSchema,
 })
